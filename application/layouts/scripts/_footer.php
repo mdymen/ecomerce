1999c7eb68f78f4d8bfaa67c789e0721788f2b77
mdymen <msn@dymenstein.com>
2015-07-18 17:55:14 -0300

    .

--- application/layouts/scripts/_footer.php
+++ application/layouts/scripts/_footer.php
@@ -2,45 +2,43 @@
     	<div class="container">
         	<div class="col-lg-3 col-md-3 col-sm-6">
         		<div class="column">
-        			<h4>Information</h4>
+        			<h4>Informação</h4>
         			<ul>
-        				<li><a href="about.html">About Us</a></li>
-        				<li><a href="typography.html">Policy Privacy</a></li>
-        				<li><a href="typography.html">Terms and Conditions</a></li>
-        				<li><a href="typography.html">Shipping Methods</a></li>
+        				<li><a href="about.html">Sobre nós</a></li>
+        				<li><a href="typography.html">Políticas de privacidade</a></li>
+        				<li><a href="typography.html">Termos e condições</a></li>
+        				<li><a href="typography.html">Métodos de envío</a></li>
         			</ul>
         		</div>
         	</div>
         	<div class="col-lg-3 col-md-3 col-sm-6">
         		<div class="column">
-        			<h4>Categories</h4>
+        			<h4>Categorias</h4>
         			<ul>
-        				<li><a href="catalogue.html">Cras justo odio</a></li>
-        				<li><a href="catalogue.html">Dapibus ac facilisis in</a></li>
-        				<li><a href="catalogue.html">Morbi leo risus</a></li>
-        				<li><a href="catalogue.html">Porta ac consectetur ac</a></li>
+        				<li><a href="catalogue.html">Camisetas</a></li>
+        				<li><a href="catalogue.html">Canecas</a></li>
+        				<li><a href="catalogue.html">Chinelos</a></li>
+        				<li><a href="catalogue.html">Adhesivos</a></li>
         			</ul>
         		</div>
         	</div>
         	<div class="col-lg-3 col-md-3 col-sm-6">
         		<div class="column">
-        			<h4>Customer Service</h4>
+        			<h4>Suporte</h4>
         			<ul>
-                        <li><a href="contact.html">Contact Us</a></li>
-                        <li><a href="#">YM: cs_</a></li>
-                        <li><a href="#">Phone: +6281234567891</a></li>
-                        <li><a href="#">Email: cs.domain@domain.tld</a></li>
+                        <li><a href="contact.html">Contato</a></li>
+                        <li><a href="#">Telefone</a></li>
+                        <li><a href="#">Email</a></li>
+                        <li><a href="#">Endereço</a></li>
                     </ul>
                 </div>
             </div>
             <div class="col-lg-3 col-md-3 col-sm-6">
                 <div class="column">
-                    <h4>Follow Us</h4>
+                    <h4>Redes Sociais</h4>
                     <ul class="social">
-                        <li><a href="#">Google Plus</a></li>
                         <li><a href="#">Facebook</a></li>
                         <li><a href="#">Twitter</a></li>
-                        <li><a href="#">RSS Feed</a></li>
                     </ul>
                 </div>
             </div>
